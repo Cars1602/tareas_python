bff1dfe626c8aa08b95cd01dba7bacae2b2da4b2
matav13ej4as <159934455+Cars1602@users.noreply.github.com>
2025-06-09 23:37:27 -0400

    Add files via upload

---
+++ 2do.py
@@ -0,0 +1,8 @@
+print("ingrese un número para la tabla de multiplicar")
+numero = int(input("Número: "))
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+# Ejemplo de uso:
+# numero = 5
+3
